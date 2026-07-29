8aa655d295c68bf2db0f3bfbbb92e89be8b62434
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-07-29 21:44:52 +0800

    added door, floor, and omori sprites

---
+++ index.js
@@ -0,0 +1,36 @@
+import { resources } from '/src/resource.js';
+import { Sprite } from '/src/sprite.js';
+import { Vector2 } from '/src/vector2.js'
+
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext("2d");
+
+const floor = new Sprite({
+    resource: resources.images.floor,
+    frameSize: new Vector2(320, 180)
+})
+
+const door = new Sprite({
+    resource: resources.images.door,
+    frameSize: new Vector2(320, 180)
+})
+
+const omori = new Sprite({
+    resource: resources.images.omori,
+    frameSize: new Vector2(32, 32),
+    hFrames: 3,
+    vFrames: 4,
+    frame: 1
+})
+
+const omoriPos = new Vector2(32 * 1, 32 * 2)
+
+const draw = () => {
+    floor.drawImage(ctx, 0, 0);
+    door.drawImage(ctx, 0, 0);
+    omori.drawImage(ctx, omoriPos.x, omoriPos.y)
+}
+
+setInterval(() => {
+    draw();
+}, 300)
---
+++ src/resource.js
@@ -0,0 +1,30 @@
+class Resources {
+    constructor() {
+        // plan to download
+        this.toLoad = {
+            floor: "assets/floor.png",
+            door: "assets/door.png",
+            laptop: "assets/laptop.png",
+            lightbulb: "assets/lightbulb.png",
+            notebook: "assets/notebook.png",
+            tissues: "assets/tissues.png",
+            omori: "assets/omori-spritesheet.png"
+        };
+        // keep all of the images
+        this.images = {};
+        // load each image
+        Object.keys(this.toLoad).forEach(key => {
+            const img = new Image();
+            img.src = this.toLoad[key];
+            this.images[key] = {
+                image: img,
+                isLoaded: false
+            }
+            img.onload = () => {
+                this.images[key].isLoaded = true;
+            }
+        })
+    }
+}
+// create one instance for the whole app to use
+export const resources = new Resources();
