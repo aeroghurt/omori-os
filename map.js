ca3d7c5c4ecb3c396de8d5587f1ffd0cc34fa323
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-08-17 21:39:12 +0800

    removed all redundant files

--- map.js
+++
@@ -1,9 +0,0 @@
-export const walls = new Set();
-
-walls.add(`96,0`); // door
-for (let i = 0; i < 32; i++) {
-    walls.add(`${96 - i},${0}`)
-    walls.add(`${96},${0 - i}`)
-}
-walls.add(`128,64`); // laptop
-walls.add(`192,128`); // tissues
--- moveTowards.js
+++
@@ -1,6 +0,0 @@
-export function moveTowards(person, destinationPosition, speed) {
-    let distanceX = destinationPosition.x - person.position.x;
-    let distanceY = destinationPosition.y - person.position.y;
-    let distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
-    return distance;
-}
